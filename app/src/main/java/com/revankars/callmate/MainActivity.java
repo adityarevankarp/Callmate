551b05980b4a3babb87b3d81b1c36f929428561c
Aditya Revankar <adityarevankar03@gmail.com>
2023-03-17 00:09:42 +0530

    Added Recycler View

--- app/src/main/java/com/revankars/callmate/MainActivity.java
+++ app/src/main/java/com/revankars/callmate/MainActivity.java
@@ -1,18 +1,28 @@
 package com.revankars.callmate;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     RecyclerView recyclerView;
     TextView welcomeTextView;
     EditText messageEditText;
     ImageButton sendButton;
+
+    List<Message> messageList;
+
+    MessageAdapter messageAdapter;
 
 
 
@@ -22,9 +32,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        messageList = new ArrayList<>();
+
         recyclerView = findViewById(R.id.recycler_view);
         welcomeTextView = findViewById(R.id.welcome_text);
         messageEditText = findViewById(R.id.message_edit_text);
         sendButton = findViewById(R.id.send_btn);
+
+        //setting up recycler view;
+        messageAdapter = new MessageAdapter(messageList);
+        recyclerView.setAdapter(messageAdapter);
+        LinearLayoutManager llm = new LinearLayoutManager(this);
+        llm.setStackFromEnd(true);
+        recyclerView.setLayoutManager(llm);
+
+
+        sendButton.setOnClickListener((v)->{
+            String question = messageEditText.getText().toString().trim();
+
+            messageEditText.setText("");
+            Toast.makeText(this,question, Toast.LENGTH_SHORT).show();
+
+
+        });
+    }
+
+    void addToChat(String message,String sentBy){
+        messageList.add(new Message(message,sentBy));
+        messageAdapter.notifyDataSetChanged();
+        recyclerView.smoothScrollToPosition(messageAdapter.getItemCount());
     }
 }
