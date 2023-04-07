871a8abdc8eb71d5daa632ba7c40ff551d3efbfc
Aditya Revankar <adityarevankar03@gmail.com>
2023-04-07 22:57:30 +0530

    added new activity for whisper api

---
+++ app/src/main/java/com/revankars/callmate/TranscriptionActivity.java
@@ -0,0 +1,14 @@
+package com.revankars.callmate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TranscriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transcription);
+    }
+}
